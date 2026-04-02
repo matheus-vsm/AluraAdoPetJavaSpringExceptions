fed33fc3f5292ec4220d20f03265941742cf276a
matheus-vsm <scompinha@hotmail.com>
2026-04-02 20:31:25 -0300

    aula 4.4

--- src/main/java/adopet/api/service/PetService.java
+++ src/main/java/adopet/api/service/PetService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -23,7 +24,7 @@ public class PetService {
         return repository.findAll().stream().map(PetDTO::new).toList();
     }
 
-    public void cadastrar(CadastroPetDTO dto, MultipartFile imagem) {
+    public void cadastrar(CadastroPetDTO dto, MultipartFile imagem) throws IOException {
 
         String nomeImagem = imagemService.upload(imagem);
 
